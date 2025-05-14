1eefc72a64a92d0bfd0208349c647941f9d5ff8f
AuraAdvance <marcelo.arancibia.soto3@gmail.com>
2025-05-14 14:03:36 -0400

    Prueba despliegue automático con GitHub Action

---
+++ FcnoLimit/src/config/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAV1VGpo7qrby9CJ80VnpQo hPr3V_8J4U",
+  authDomain: "fcnolimit.firebaseapp.com",
+  projectId: "fcnolimit",
+  storageBucket: "fcnolimit.firebasestorage.app",
+  messagingSenderId: "15157650709",     
+  appId: "1:15157650709:web:7e1d2e794ba894b3b7b51c"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
